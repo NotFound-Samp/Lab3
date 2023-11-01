e6c80bb14d0c69fa361b656cc0264fec2950dd97
NotFound <salt-sasha65@yandex.ru>
2023-11-01 13:16:39 +0300

    01.11.23

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,46 +1,39 @@
-﻿using System;
-using System.IO;
+﻿using System.Diagnostics;
 using System.Threading;
-using System.Diagnostics;
 
 namespace Lab3
 {
     class Program
     {
-        private static readonly string filePath = Path.Combine(Directory.GetCurrentDirectory(), "C:\\Users\\Alex\\VS\\Lab3\\Lab3\\writerText.txt");
-        private static Barrier barrier = new Barrier(11);
-        private static Stopwatch stopwatch = new Stopwatch();
-
+        private static readonly string filePath =
+            Path.Combine(Directory.GetCurrentDirectory(), "C:\\Users\\Alex\\VS\\Lab3\\Lab3\\writerText.txt");
+        private static int currentThread = 1;
+        private static object lockObject = new object();
         static void Main()
         {
             for (int i = 1; i <= 10; i++)
             {
-                Thread thread = new Thread(new ParameterizedThreadStart(PerformTask));
+                Thread thread = new Thread(PrintData);
                 thread.Start(i);
             }
-
-            barrier.SignalAndWait();
-            Console.WriteLine("Все потоки завершили работу");
         }
-
-        static void PerformTask(object threadNumber)
+        static void PrintData(object threadNumber)
         {
-            
-            stopwatch.Start();
-            barrier.SignalAndWait();
-            stopwatch.Stop();
-            double executionTime = stopwatch.Elapsed.TotalMilliseconds;
-
-            try
+            int number = (int)threadNumber;
+            while (true)
             {
-                lock(typeof(Program))
-                    using (StreamWriter writer = new StreamWriter(filePath, true))
-                        writer.WriteLine($"Поток {(int)threadNumber} завершил работу. Длительность выполнения: {executionTime} мс. Текущее время: {DateTime.Now}");
-                Thread.Sleep(1000);
-            }
-            finally
-            {
-                barrier.SignalAndWait();
+                lock (lockObject)
+                {
+                    if (number == currentThread)
+                    {
+                        using (StreamWriter writer = new StreamWriter(filePath, true))
+                        {
+                            writer.WriteLine($"Поток {(int)threadNumber} запустился. Текущее время: {DateTime.Now}");
+                        }
+                        currentThread = (currentThread % 10) + 1;
+                    }
+                }
+                Thread.Sleep(10);
             }
         }
     }
